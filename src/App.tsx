fc936b6f2d47a2c297eb3880f6bccd063f7b1c3d
Nipun Avishka De Zoysa <nipunavishka123@gmail.com>
2024-06-24 23:56:27 +0530

    v1.1.1 update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import "./App.css";
 import { showConfirm } from "react-confirm-prompt";
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState("");
   return (
     <>
+    <h1>{result}</h1>
       <button
+        className="gg"
         onClick={() =>
           showConfirm("Are you sure?", {
             description:
               "This action cannot be undone. All values associated with this field will be lost.",
             type: "warning",
+          }).then((res) => {
+            if (res) {
+              setResult("Confirmed");
+            } else {
+              setResult("Canceled");
+            }
           })
         }
       >
