ce86ee5b3e3ac13ce133d3c8bcfa5ef1c0fa04da
Nipun Avishka De Zoysa <nipunavishka123@gmail.com>
2024-06-24 23:39:31 +0530

    v1.1.0 updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import './App.css'
-import { showConfirm } from 'react-confirm-prompt'
+import "./App.css";
+import { showConfirm } from "react-confirm-prompt";
 
 function App() {
-
   return (
     <>
-    <button onClick={() => showConfirm('Are you sure?')}>Show Confirm Box</button>
+      <button
+        onClick={() =>
+          showConfirm("Are you sure?", {
+            description:
+              "This action cannot be undone. All values associated with this field will be lost.",
+            type: "warning",
+          })
+        }
+      >
+        Show Confirm Box
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
