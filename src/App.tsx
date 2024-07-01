b5593cf425662e6be0b9bc9ccce67423d518ecf6
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-02 00:13:41 +0530

    installation added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { FaGithub, FaNpm } from "react-icons/fa";
 import PlayGround from "./PlayGround";
 import CustomIcons from "./CustomIcons";
 import Intro from "./Intro";
+import { CopyBlock, dracula } from "react-code-blocks";
 function App() {
-  
   return (
     <div className="w-full flex justify-center">
       <div className="relative w-full max-w-[1920px] min-h-screen bg-gray-800 text-blue-200 flex flex-col gap-10">
-        <div className="w-full py-4 bg-gray-900 flex justify-center">
+        <div className="w-full py-4 bg-gray-900 flex justify-center sticky top-0 z-50 opacity">
           <div className="main-container flex justify-between items-center">
             <h1 className="text-lg font-semibold">react-confirm-prompt</h1>
             <div className="flex gap-4 items-center ">
@@ -27,8 +27,22 @@ function App() {
             </div>
           </div>
         </div>
-        <Intro/>
+
+        <Intro />
+
+        <div className="main-container flex flex-col gap-2">
+          <h1 className="text-2xl font-semibold">Installation</h1>
+          <CopyBlock
+            text={`npm i react-confirm-prompt`}
+            language={""}
+            showLineNumbers={true}
+            theme={dracula}
+            codeBlock
+          />
+        </div>
+
         <PlayGround />
+
         <CustomIcons />
       </div>
     </div>
--- src/Intro.tsx
+++ src/Intro.tsx
@@ -23,7 +23,13 @@ export default function Intro() {
           </button>
           <div>VS</div>
           <button
-            onClick={() => showConfirm("Hi")}
+            onClick={() =>
+              showConfirm("Hi! I am a confirm dialog.", {
+                description:
+                  "Elevate your application's UX with ReactConfirmPrompt",
+                  type: "success"
+              })
+            }
             className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 hover:border-[#646cff]"
           >
             React Confirm Promt
