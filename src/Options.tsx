9fa601866e9f1c2bdb74660c9d014ebc89113ec1
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-03 10:17:25 +0530

    Update Options.tsx

--- src/Options.tsx
+++ src/Options.tsx
@@ -7,12 +7,96 @@ const values = [
     des: "pre-defined types(themes) of confirmation box. 'info' is the defualt value",
     ty: `string ("info", "warning", "success", "question")`,
   },
+  {
+    op: "animation",
+    des: "confirm box animations. 'scale' is the defualt value",
+    ty: `string ("none", "scale", "fade",  "blur", "slide-up", "slide-down", "slide-right", "slide-left")`,
+  },
+  {
+    op: "confirmLabel",
+    des: "custom text that should display in confirm button",
+    ty: `string`,
+  },
+  {
+    op: "confirmColor",
+    des: "confirm button color",
+    ty: `string (hex or rgb color codes)`,
+  },
+  {
+    op: "confirmTextColor",
+    des: "confirm button text color",
+    ty: `string (hex or rgb color codes)`,
+  },
+  {
+    op: "cancelLabel",
+    des: "custom text that should display in cancel button",
+    ty: `string`,
+  },
+  {
+    op: "cancelColor",
+    des: "cancel button color",
+    ty: `string (hex or rgb color codes)`,
+  },
+  {
+    op: "cancelTextColor",
+    des: "cancel button text color",
+    ty: `string (hex or rgb color codes)`,
+  },
+  {
+    op: "hideCancel",
+    des: "hiding cancel button",
+    ty: `boolean`,
+  },
+  {
+    op: "disableBlur",
+    des: "disabling background click cancellation",
+    ty: `boolean`,
+  },
+  {
+    op: "color",
+    des: "define color for both icon & confirm button",
+    ty: `string (hex or rgb color codes)`,
+  },
+  {
+    op: "hoverColor",
+    des: "define button hover color",
+    ty: `string (hex or rgb color codes)`,
+  },
+  {
+    op: "hoverTextColor",
+    des: "define button's hover text color",
+    ty: `string (hex or rgb color codes)`,
+  },
+  {
+    op: "icon",
+    des: "define custom icon",
+    ty: `JSX.Element`,
+  },
+  {
+    op: "iconColor",
+    des: "define icon color",
+    ty: `string (hex or rgb color codes)`,
+  },
+  {
+    op: "hideBackground",
+    des: "hiding dark background",
+    ty: `boolean`,
+  },
+  {
+    op: "hideShadow",
+    des: "hiding confirm box shadow",
+    ty: `boolean`,
+  },
 ];
 
 export default function Options() {
   return (
-    <div className="main-container overflow-x-auto relative">
-      <h1 className="text-2xl font-semibold">Options</h1>
+    <div className="main-container overflow-x-auto relative flex flex-col gap-2">
+      <div>
+        <h1 className="text-2xl font-semibold">Options</h1>
+        <p>
+          You can customize the confirm box with the following options,</p>
+      </div>
       <table className="w-full text-sm text-left rtl:text-right  text-gray-400 border-gray-700 border">
         <thead className="text-xs  uppercase  bg-gray-700 text-gray-400">
           <tr>
